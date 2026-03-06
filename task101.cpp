e164e7b4e6ce992963d188f7501d5ef08dfdd107
Abid Hasan <abidhasanutsha@gmail.com>
2026-03-06 14:03:39 +0600

    fixed title

--- task101.cpp
+++ task101.cpp
@@ -80,7 +80,7 @@ int main(int argc, char* argv[])
         SDL_RenderClear(renderer);
 
         
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 139, 139, 0);
 
         drawCircle(renderer, cx, cy, radius);
 
